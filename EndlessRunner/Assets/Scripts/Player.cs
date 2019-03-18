dd429b899f424c4e2e218d2a59297e6e0cde7252
Julcoq <jessica@lithodomosvr.com>
2019-03-18 12:14:15 +1100

    Added: Background, Spawner

--- EndlessRunner/Assets/Scripts/Player.cs
+++ EndlessRunner/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -15,6 +16,11 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager. GetActiveScene().buildIndex);
+        }
+
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed*Time.deltaTime);
 
         if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
